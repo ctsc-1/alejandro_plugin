3920ef4918ef7c2f75cd7cc79f92525a05db78ce
Club Costa Tropical <160325983+ctsc-1@users.noreply.github.com>
2025-01-18 21:13:34 +0100

    Delete alejandro-monitor directory

--- alejandro-monitor/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/tests/js/setup.js'],
-    moduleNameMapper: {
-        '\\.(css|less|scss)$': '<rootDir>/tests/js/__mocks__/styleMock.js'
-    }
-}; 
--- alejandro-monitor/tests/test-class-alejandro-monitor.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Test_Alejandro_Monitor extends WP_UnitTestCase {
-    public function test_plugin_initialized() {
-        $instance = Alejandro_Monitor::get_instance();
-        $this->assertInstanceOf(Alejandro_Monitor::class, $instance);
-    }
-} 
